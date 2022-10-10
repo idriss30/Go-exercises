19b0796b97b17b4700b90a0537c3c1c613b47130
idriss30 <idriss.cissoko@hotmail.com>
2022-10-10 14:31:07 -0400

    exercises update 6-4

--- ch1.go
+++ ch1.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
 	// printZero()
 	//returnInteger()
-	
-	
-
+	//displayAlphabet()
+	//print0To100Sum()
+	//loopDivisibleBy50()
+	//rangeDivisibleBy50()
+	//promptInteger()
 }
 
 // resulting in zero 4-1
@@ -48,12 +51,108 @@ func currentValue() {
 
 }
 
+//Using for loops and string functions, create a computer program that displays the alphabet from A to Z.
 
-//Using for loops and string functions, create a computer program that displays the alphabet from A to Z. 
+func displayAlphabet() {
+	alphabet := string("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	for i := 0; i < len(alphabet); i++ {
+		fmt.Printf("%v\n", string(alphabet[i])) // important to convert to string first.
+	}
+}
 
-func displayAlphabet(){
-	 alphabet := string("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-	 for i := 0 ; i < len(alphabet); i++ {
-		fmt.Printf("%v", alphabet[i])
-	 }
+//Create a program that computes the sum of all numbers between 0 and 100
+
+func print0To100Sum() {
+	sum := 0
+	for i := 1; i < 100; i++ { // range is ( 1 - 99)
+		sum += i
+	}
+	fmt.Printf("the sum of all numbers between 0 and 100 is %v", sum)
+}
+
+//Write two programs, each of which displays all numbers divisible by 50 between 100 and 1,000 (inclusive).
+//Both programs should have identical output.
+
+//Use the range keyword with for in one program.
+//Use for without range in the other program.
+
+func loopDivisibleBy50() {
+	for i := 100; i <= 1000; i++ {
+		if i%50 == 0 {
+			fmt.Printf("%v is divisible by 50\n", i)
+		}
+	}
+}
+
+func rangeDivisibleBy50() {
+	for v := range [1001]int{} {
+		if v < 100 {
+			continue
+		}
+		if v%50 == 0 {
+			fmt.Printf("%v, is divisible by 50\n", v)
+		}
+	}
+
+}
+
+//Create a program that prompts the user for an integer and then displays the following information about that number:
+//The number of digits in the value entered
+//The first and last digits of the number
+//The sum of the digits in the number
+//The product of the digits in the number
+//Whether or not the number is prime
+//The factorial of the number
+
+func promptInteger() {
+	fmt.Println("please enter a number")
+	var userInput string
+	fmt.Scan(&userInput)
+	var userInputToNumber int
+	userInputToNumber, _ = strconv.Atoi(userInput)
+
+	fmt.Printf("the number of digits is %v\n", len(userInput))
+	firstDigit := string(userInput[0])
+	lastDigit := string(userInput[int(len(userInput))-1]) // get the value as string but the last item converting string to int
+	fmt.Printf("the first digit is %v and the last one is %v\n", firstDigit, lastDigit)
+
+	var stringToNumber int
+	var sum = 0
+	var digitMultiplication = 1
+
+	for i := 0; i < len(userInput); i++ {
+		stringToNumber, _ = strconv.Atoi(string(userInput[i]))
+		sum += stringToNumber
+		digitMultiplication *= stringToNumber
+	}
+
+	fmt.Printf("the sum of all the digits is %v\n", sum)
+	fmt.Printf("the product of the digits is %v\n", digitMultiplication)
+
+	if userInputToNumber%2 != 0 && sum/3 != 0 {
+		fmt.Printf("%v is a prime number\n", userInput)
+	} else {
+		fmt.Printf("%v is not a prime number\n", userInput)
+
+	}
+	// factorial;
+	var factorial int = 1
+	for i := 1; i < userInputToNumber; i++ {
+		j := i + 1
+
+		if j > userInputToNumber {
+			break
+		}
+		factorial *= j
+
+	}
+
+	fmt.Printf("%v factorial is %v", userInputToNumber, factorial)
+
+}
+
+//Write a program that asks the user for an input integer and then computes the reverse of that number 
+
+func reserseUserNumber(){
+
 }
